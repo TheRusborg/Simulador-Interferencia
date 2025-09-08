91cd56d38cb8d707dc64b9d986c49d3e6a3593e6
TheRusborg <164403100+TheRusborg@users.noreply.github.com>
2025-09-08 12:06:12 +0200

    Update Interferencia.py

--- Interferencia.py
+++ Interferencia.py
@@ -6,7 +6,7 @@ import matplotlib.widgets as widgets
 # Se definen los parámetros necesarios para los cálculos
 # Todas las unidades están en Sistema Internacional.
 screen_width = 0.3
-screen_height = 0.02
+screen_height = 0.001
 
 # I_o es la intensidad máxima (en el zero).
 I_o = 10
@@ -33,11 +33,10 @@ def Intensidad(x, wavelength=wavelength, screenDist=screenDist,
     intensidad = I_o*((np.cos((np.pi*slitSeparation*np.sin(alpha))/wavelength))**2)*((np.sin(np.pi*slitWidth*np.sin(alpha)/wavelength))/(np.pi*slitWidth*np.sin(alpha)/wavelength))**2
     return intensidad
 
-
  
 # Se prepara y se hace el plot de los datos.
 # Se define la resolución de la imagen. Es la cantidad de puntos relativa al ancho y alto de la pantalla.
-res = 2000
+res = 3000
 m, n = int(2*screen_width*res), int(2*screen_height*res)
 x = np.linspace(-screen_width, screen_width, m)
 y = np.linspace(-screen_height, screen_height, n)
@@ -52,10 +51,13 @@ for i,val in enumerate(x):
 # Se usa la función meshgrid para hacer dos matrices con todos los puntos distribuidos.
 xx,yy=np.meshgrid(x,y)
 
-# Se preparan los subplots.
+# Se preparan los subplots y los ejes.
 fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.3)
+plt.subplots_adjust(bottom=0.25)
 
+plt.title('Patrón de difracción')
+ax.set_xlabel('x (m)')
+ax.set_ylabel('y (m)')
 
 # Se hace un "heatmap" usando las matrices obtenidas con meshgrid y se le asigna a cada posición una intensidad.
 # El método .T transpone la matriz.
@@ -64,85 +66,42 @@ heatmap = ax.pcolormesh(xx.T, yy.T, arr_intensidad, cmap ='Greens_r')
 # Se crea una barra con la escala de color del "heatmap".
 plt.colorbar(heatmap, orientation='horizontal')
 
+
 # Sliders
-ax_slider1 = plt.axes([0.18, 0.24, 0.65, 0.03])  # [left, bottom, width, height]
-wavelength_slider = widgets.Slider(ax_slider1, "Wavelength", 380*10**(-10), 700*10**(-10), valinit=wavelength)
+ax_slider1 = plt.axes([0.18, 0.21, 0.65, 0.03])  # [left, bottom, width, height]
+wavelength_slider = widgets.Slider(ax_slider1, "Wavelength [m]", 380*10**(-10), 700*10**(-10), valinit=wavelength)
 
 
-ax_slider2 = plt.axes([0.18, 0.17, 0.65, 0.03])  # [left, bottom, width, height]
-screen_slider = widgets.Slider(ax_slider2, "Screen distance", 0.1, 5, valinit=screenDist)
+ax_slider2 = plt.axes([0.18, 0.15, 0.65, 0.03])  # [left, bottom, width, height]
+screen_slider = widgets.Slider(ax_slider2, "Screen distance [m]", 0.1, 5, valinit=screenDist)
 
 
-ax_slider3 = plt.axes([0.18, 0.10, 0.65, 0.03])  # [left, bottom, width, height]
-slitSeparation_slider = widgets.Slider(ax_slider3, "Slit separation", 0.000001, 0.00001, valinit=slitSeparation)
+ax_slider3 = plt.axes([0.18, 0.09, 0.65, 0.03])  # [left, bottom, width, height]
+slitSeparation_slider = widgets.Slider(ax_slider3, "Slit separation [m]", 0.000001, 0.00001, valinit=slitSeparation)
 
 
 ax_slider4 = plt.axes([0.18, 0.03, 0.65, 0.03])  # [left, bottom, width, height]
-width_slider = widgets.Slider(ax_slider4, "Slit width", 0.0000001, 0.000001, valinit=slitWidth)
-
+width_slider = widgets.Slider(ax_slider4, "Slit width [m]", 0.0000001, 0.000001, valinit=slitWidth)
 
 
 
 # Función de actualización
-def update_wavelength(val,m=m, n=n, width=screen_width, height=screen_height):
+def update(val,m=m, n=n, width=screen_width):
     x = np.linspace(-width, width, m)
-    y = np.linspace(-height, height, n)
-    a = wavelength_slider.val
+    
     arrIntensidad_new = np.zeros((m,n))
     for i,valor in enumerate(x):
         for j in range(n):
-            intensidad = Intensidad(valor, wavelength=a)/4*I_o
+            intensidad = Intensidad(valor, wavelength=wavelength_slider.val, screenDist=screen_slider.val, slitSeparation=slitSeparation_slider.val, slitWidth=width_slider.val)/4*I_o
             arrIntensidad_new[i][j] = intensidad
     
     heatmap.set_array(arrIntensidad_new.ravel())  # actualizar los valores
     fig.canvas.draw_idle()
 
-
-def update_screenDistance(val,m=m, n=n, width=screen_width, height=screen_height):
-    x = np.linspace(-width, width, m)
-    y = np.linspace(-height, height, n)
-    a = screen_slider.val
-    arrIntensidad_new = np.zeros((m,n))
-    for i,valor in enumerate(x):
-        for j in range(n):
-            intensidad = Intensidad(valor, screenDist=a)/4*I_o
-            arrIntensidad_new[i][j] = intensidad
     
-    heatmap.set_array(arrIntensidad_new.ravel())  # actualizar los valores
-    fig.canvas.draw_idle()
-    
-    
-def update_slitSeparation(val,m=m, n=n, width=screen_width, height=screen_height):
-    x = np.linspace(-width, width, m)
-    y = np.linspace(-height, height, n)
-    a = slitSeparation_slider.val
-    arrIntensidad_new = np.zeros((m,n))
-    for i,valor in enumerate(x):
-        for j in range(n):
-            intensidad = Intensidad(valor, slitSeparation=a)/4*I_o
-            arrIntensidad_new[i][j] = intensidad
-    
-    heatmap.set_array(arrIntensidad_new.ravel())  # actualizar los valores
-    fig.canvas.draw_idle()
-    
-    
-def update_slitWidth(val,m=m, n=n, width=screen_width, height=screen_height):
-    x = np.linspace(-width, width, m)
-    y = np.linspace(-height, height, n)
-    a = width_slider.val
-    arrIntensidad_new = np.zeros((m,n))
-    for i,valor in enumerate(x):
-        for j in range(n):
-            intensidad = Intensidad(valor, slitWidth=a)/4*I_o
-            arrIntensidad_new[i][j] = intensidad
-    
-    heatmap.set_array(arrIntensidad_new.ravel())  # actualizar los valores
-    fig.canvas.draw_idle()
-    
-    
-wavelength_slider.on_changed(update_wavelength)
-screen_slider.on_changed(update_screenDistance) 
-slitSeparation_slider.on_changed(update_slitSeparation) 
-width_slider.on_changed(update_slitWidth) 
+wavelength_slider.on_changed(update)
+screen_slider.on_changed(update) 
+slitSeparation_slider.on_changed(update) 
+width_slider.on_changed(update) 
 
 plt.show()
